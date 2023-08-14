944bdea221653b7561b4d7d8250fd78fd0797eec
csupreme19 <csupreme19@gmail.com>
2023-08-14 17:05:08 +0900

    Mockito 적용

--- src/test/java/csh/studytobyspring/dao/MemberDaoTest.java
+++ src/test/java/csh/studytobyspring/dao/MemberDaoTest.java
@@ -2,42 +2,54 @@ package csh.studytobyspring.dao;
 
 import csh.studytobyspring.constant.Level;
 import csh.studytobyspring.model.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+@ExtendWith(MockitoExtension.class)
 class MemberDaoTest {
 
-    static class MockMemberDao extends MemberDao {
-        private Map<Long, Member> members = new HashMap<>();
-
-        @Override
-        public Member add(Member member) {
-            members.put(member.getId(), member);
-            return member;
-        }
-
-        @Override
-        public List<Member> getAll() {
-            return members.values().stream().toList();
-        }
-    }
+    @Mock
+    MemberDao dao;
 
     @Test
     @DisplayName("멤버 조회 테스트")
     void getMember() {
-        MemberDao dao = new MockMemberDao();
         Member member = new Member(1L, "최승훈", 10, Level.BRONZE);
+
+        BDDMockito.given(dao.getAll()).willReturn(List.of(member));
+        BDDMockito.given(dao.add(member)).willReturn(member);
+
         dao.add(member);
         Member foundMember = dao.getAll().get(0);
+        
+        Assertions.assertThat(member.getName()).isEqualTo(foundMember.getName());
+        Assertions.assertThat(member.getAge()).isEqualTo(foundMember.getAge());
 
-        assertThat(member.getName()).isEqualTo(foundMember.getName());
-        assertThat(member.getAge()).isEqualTo(foundMember.getAge());
+        BDDMockito.verify(dao, Mockito.times(1)).add(member);
+        BDDMockito.verify(dao, Mockito.times(1)).getAll();
     }
+
+    //    static class MockMemberDao extends MemberDao {
+//        private Map<Long, Member> members = new HashMap<>();
+//
+//        @Override
+//        public Member add(Member member) {
+//            members.put(member.getId(), member);
+//            return member;
+//        }
+//
+//        @Override
+//        public List<Member> getAll() {
+//            return members.values().stream().toList();
+//        }
+//    }
 
 }
