07446b83d430b593b586e9f0959cb0ce69fcded1
csupreme19 <csupreme19@gmail.com>
2023-08-14 17:50:29 +0900

    트랜잭션 포인트컷, 어드바이저

--- src/main/java/csh/studytobyspring/Client.java
+++ src/main/java/csh/studytobyspring/Client.java
@@ -1,9 +1,12 @@
 package csh.studytobyspring;
 
+import csh.studytobyspring.advice.TransactionAdvice;
 import csh.studytobyspring.service.MemberService;
 import csh.studytobyspring.service.MemberServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,7 +18,18 @@ public class Client {
     void call() {
         ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
         proxyFactoryBean.setTarget(memberService);
+
+        TransactionAdvice advice = new TransactionAdvice();
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedName("get*");
+
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, advice);
+
+        proxyFactoryBean.addAdvisor(advisor);
+
         MemberService txMemberService = (MemberService) proxyFactoryBean.getObject();
+
+
     }
 
 
