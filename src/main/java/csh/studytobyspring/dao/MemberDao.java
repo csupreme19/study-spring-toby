eca99e2420f490092c40468e622369e36b106a9d
csupreme19 <csupreme19@gmail.com>
2023-08-14 11:09:49 +0900

    사라진 예외, 언체크 예외 처리의 생략

--- src/main/java/csh/studytobyspring/dao/MemberDao.java
+++ src/main/java/csh/studytobyspring/dao/MemberDao.java
@@ -1,15 +1,25 @@
 package csh.studytobyspring.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
 public class MemberDao {
 
-    public void executeSql(final String query) throws SQLException {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    private JdbcContext jdbcContext;
+
+    public void deleteAll() throws SQLException {
         this.jdbcContext.executeSql("delete from members");
     }
 
-    private JdbcContext jdbcContext;
+    public void deleteAllJdbcTemplate() {
+        this.jdbcTemplate.update("delete from members");
+    }
 
     public void setJdbcContext(DataSource dataSource) {
         this.jdbcContext = new JdbcContext();
--- src/main/java/csh/studytobyspring/dao/MySqlConnection.java
+++ src/main/java/csh/studytobyspring/dao/MySqlConnection.java
@@ -1,5 +1,17 @@
 package csh.studytobyspring.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MySqlConnection implements Connection {
 
+    @Override
+    public PreparedStatement prepareStatement(String stmt) {
+        return null;
+    }
+
+    @Override
+    public void close() throws SQLException {
+
+    }
 }
