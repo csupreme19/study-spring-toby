fafff0f767e87c8d7324f1a5be68ebfea8cdee09
csupreme19 <csupreme19@gmail.com>
2023-08-14 17:18:21 +0900

    데코레이터 패턴 살펴보기

--- src/main/java/csh/studytobyspring/StudyTobySpringApplication.java
+++ src/main/java/csh/studytobyspring/StudyTobySpringApplication.java
@@ -3,11 +3,20 @@ package csh.studytobyspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 @SpringBootApplication
 public class StudyTobySpringApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         SpringApplication.run(StudyTobySpringApplication.class, args);
+
+        // 데코레이터 패턴
+        InputStream is = new BufferedInputStream(new FileInputStream("hello.txt"));
+
     }
 
 }
