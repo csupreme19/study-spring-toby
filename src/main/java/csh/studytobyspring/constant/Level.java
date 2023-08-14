328756830d8d33266adbe4a62c1d4ebf4b88d221
csupreme19 <csupreme19@gmail.com>
2023-08-14 14:11:45 +0900

    상수 정의

---
+++ src/main/java/csh/studytobyspring/constant/Level.java
@@ -0,0 +1,9 @@
+package csh.studytobyspring.constant;
+
+public abstract class Level {
+
+    private static final int BRONZE = 1;
+    private static final int SILVER = 2;
+    private static final int GOLD = 3;
+
+}
