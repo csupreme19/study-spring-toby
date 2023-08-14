17ddde8d81adfc1fd92c8c4f9990eec934636911
csupreme19 <csupreme19@gmail.com>
2023-08-14 17:41:26 +0900

    스프링의 프록시 팩토리 빈

--- src/main/java/csh/studytobyspring/Client.java
+++ src/main/java/csh/studytobyspring/Client.java
@@ -1,24 +1,21 @@
 package csh.studytobyspring;
 
-import csh.studytobyspring.config.TransactionProxyFactoryBean;
 import csh.studytobyspring.service.MemberService;
 import csh.studytobyspring.service.MemberServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationContext;
+import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class Client {
 
-    private final ApplicationContext ctx;
-
     private MemberServiceImpl memberService;
 
     void call() {
-        TransactionProxyFactoryBean txProxyFactoryBean = ctx.getBean("&memberService", TransactionProxyFactoryBean.class);
-        txProxyFactoryBean.setTarget(memberService);
-        MemberService txMemberService = (MemberService) txProxyFactoryBean.getObject();
+        ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+        proxyFactoryBean.setTarget(memberService);
+        MemberService txMemberService = (MemberService) proxyFactoryBean.getObject();
     }
 
 
