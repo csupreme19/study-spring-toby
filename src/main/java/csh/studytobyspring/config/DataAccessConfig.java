7b14904862a8fca4ae43011ca37552f0b4f1b3b1
csupreme19 <csupreme19@gmail.com>
2023-08-14 17:56:10 +0900

    빈 후처리를 통한 자동 프록시 등록 빈

--- src/main/java/csh/studytobyspring/config/DataAccessConfig.java
+++ src/main/java/csh/studytobyspring/config/DataAccessConfig.java
@@ -1,14 +1,20 @@
 package csh.studytobyspring.config;
 
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class DaoFactory {
+public class DataAccessConfig {
 
     @Bean
     JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate();
+    }
+
+    @Bean
+    DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        return new DefaultAdvisorAutoProxyCreator();
     }
 
 }
