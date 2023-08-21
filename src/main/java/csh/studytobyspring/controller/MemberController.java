dc3bad0584406f44435bccaa19dcc21969b0aa16
csupreme19 <csupreme19@gmail.com>
2023-08-21 10:30:36 +0900

    XML 기반 빈 등록

---
+++ src/main/java/csh/studytobyspring/controller/MemberController.java
@@ -0,0 +1,11 @@
+package csh.studytobyspring.controller;
+
+import csh.studytobyspring.service.MemberService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+}
